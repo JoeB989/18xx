7631acf3a335c7090650d7aab65aa3dc85b305ee
JoeB989 <jtbronikowski@gmail.com>
2022-03-09 09:25:08 -0600

    fix

--- assets/app/view/game/entity_list.rb
+++ assets/app/view/game/entity_list.rb
@@ -67,9 +67,9 @@ module View
 
           owner = " (#{@game.acting_for_entity(entity).name.truncate})" if !entity.player? && entity.owner
           owner = ' (CLOSED)' if entity.closed?
-          name = entity.company? ? entity.sym : entity.name
           entity_bot = ' (bot)' if entity.player? && entity.bot?
           owner_bot = ' (bot)' if !entity.player? && entity&.owner&.bot?
+          name = entity.company? ? entity.sym : entity.name
           children << h(:span, "#{name}#{entity_bot}#{owner}#{owner_bot}")
 
           h(:li, entity_props, children)
